f43a734d1cf6bde55464b88cc15082268131f922
A00425792 <Vivekanand.Boopathy@smu.ca>
2018-12-02 19:28:02 -0400

    SQL Files
    To create the tables needed for the project.

---
+++ MiltonHotel/SQL/SQLQuery3.sql
@@ -0,0 +1,6 @@
+
+SET IDENTITY_INSERT ROOMS ON
+insert into ROOMS (ROOM_NO, ROOM_TYPE, PRICE, ROOM_NAME) values ('1',N'Single bed','80',N'Milton - Single'), ('2',N'Single bed','80',N'Milton - Single'), ('3',N'Single bed','80',N'Milton - Single'), ('4',N'Single bed','80',N'Milton - Single'),
+('5',N'Double bed','120',N'Milton - Couple'),('6',N'Double bed','120',N'Milton - Couple'),('7',N'Double bed','120',N'Milton - Couple'),('8',N'Double bed','120',N'Milton - Couple'),
+('9',N'Suite','300',N'Milton - Suite'), ('10',N'Suite','300',N'Milton - Suite'), ('11',N'Suite','300',N'Milton - Suite'), ('12',N'Suite','300',N'Milton - Suite'), ('13',N'Suite','300',N'Milton - Suite'), ('14',N'Suite','300',N'Milton - Suite'), 
+('15',N'Suite','300',N'Milton - Suite'), ('16',N'Presidential','1200',N'Milton - Presidential Suite'); 
